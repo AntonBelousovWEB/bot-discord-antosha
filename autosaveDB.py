28a4e27c41dfd4954be00e65834d49f6c745aa36
Borodach <75586389+BoRoDaTeNkiY@users.noreply.github.com>
2022-12-03 16:22:03 +0200

    Update autosaveDB.py

--- autosaveDB.py
+++ autosaveDB.py
@@ -4,7 +4,7 @@ import subprocess
 subprocess.check_call(['git', 'config', '--global', 'user.name', '"BoRoDaTeNkiY"'])
 subprocess.check_call(['git', 'config', '--global', 'user.email', '"polpoltrop228@gmail.com"'])
 subprocess.check_call(['git', 'init'])
-subprocess.check_call(['git', 'remote', 'add', 'origin', 'https://BoRoDaTeNkiY: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@github.com/BoRoDaTeNkiY/db2.git'])
+subprocess.check_call(['git', 'remote', 'add', 'origin', f'https://BoRoDaTeNkiY:{}@github.com/BoRoDaTeNkiY/db2.git'])
 
 while True:
     time.sleep(60)
